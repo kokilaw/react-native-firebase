b43242441baedbd2757208c45866ce1549b2ab89
kokilaw <kokilapc@gmail.com>
2020-09-17 10:49:56 +0530

    fix: price and quantity properties added to Item struct

--- packages/analytics/lib/structs.js
+++ packages/analytics/lib/structs.js
@@ -28,6 +28,8 @@ const Item = struct({
   item_list_name: 'string?',
   item_location_id: 'string?',
   item_variant: 'string?',
+  price: 'string?',
+  quantity: 'string?',
 });
 
 export const ScreenView = struct({
